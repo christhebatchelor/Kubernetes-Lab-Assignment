597a035921533a3b445a6666eadbf485749a4ece
Chris Batchelor <christhebatchelor@gmail.com>
2022-10-17 11:37:06 -0500

    Update views.py

--- HelloWorldApp/flask/app/views.py
+++ HelloWorldApp/flask/app/views.py
@@ -1,5 +1,10 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
 def index():
-    return '<html><h1 style="text-align: center;">Hello World from Flask!</h1></html>'
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
